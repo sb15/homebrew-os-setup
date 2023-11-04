3cd3475f4a4cecaee19428c0852f467d17246a35
Alexander Perov <alexander.perov@gmail.com>
2023-11-04 14:53:31 +0300

    v1.0.1

--- Formula/tools.rb
+++ Formula/tools.rb
@@ -1,8 +1,8 @@
 class Tools < Formula
   homepage "https://github.com/sb15/os-setup"
-  url "https://github.com/sb15/os-setup/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "ee2ce1372130c1125f964b244546c22b152fea298895e053468c4db24db94900"
-  version '1.0.1'
+  url "https://github.com/sb15/os-setup/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "a5ffd13999a4176467049b9085b274c88d5634658cf27182b08b67a8ead61265"
+  version '1.0.2'
 
   depends_on 'openssl'
   depends_on 'p7zip'
